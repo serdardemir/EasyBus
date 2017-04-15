4a41e1b80f879b1fc431bb144968e8190c4783e4
serdardemir <serdardemir@outlook.com>
2017-04-15 12:11:09 +0300

    OracleAq tests ignored
    Need Oracle DataAccess compability dll

--- EasyBus.Tests/AQIntegration/AQIntegrationTests.cs
+++ EasyBus.Tests/AQIntegration/AQIntegrationTests.cs
@@ -1,19 +1,16 @@
-﻿using EasyBus.Abstraction;
+﻿using System;
+using System.Linq;
+using EasyBus.Abstraction;
 using EasyBus.Abstraction.Contracts;
 using EasyBus.Consumer.Handlers;
 using EasyBus.Types.MessageTypes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace EasyBus.Tests.AQIntegration
 {
     public class AQIntegrationTests
     {
-        [Fact]
+        [Fact(Skip = "Need Oracle DataAccess compability dll for prevent BadImage Exception")]        
         public void Should_Publish_OracleQueue()
         {
             var container = IocBootstrapper.Instance;
@@ -25,15 +22,14 @@ namespace EasyBus.Tests.AQIntegration
             };
 
             messageEmitter.Emit(orderMessage);
-
         }
 
-        [Fact]
+        [Fact(Skip = "Need Oracle DataAccess compability dll for prevent BadImage Exception")]        
         public void Should_SubscribeOracleQueue()
         {
             var container = IocBootstrapper.Instance;
             var subscriber = container.GetInstance<ISubscriber>();
-            var handler=new ORDERMessageHandler();
+            var handler = new ORDERMessageHandler();
             subscriber.Subscribe(handler);
         }
     }
